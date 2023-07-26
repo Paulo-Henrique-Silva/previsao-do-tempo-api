a1d2b2fe758cd72bb55a15e4c50e9a6935521a61
Paulo <pshenrique2015@gmail.com>
2023-07-26 14:24:52 -0300

    Adicionando métodos de consulta repository.

---
+++ PrevisaoDoTempoAPI/Repositories/ConsultaRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrevisaoDoTempoAPI.Data;
+using PrevisaoDoTempoAPI.Interfaces;
+using PrevisaoDoTempoAPI.Models;
+using System;
+
+namespace PrevisaoDoTempoAPI.Repositories
+{
+    public class ConsultaRepository : IConsultaRepository
+    {
+        private readonly DataContext dataContext;
+
+        public ConsultaRepository(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task<Consulta> Adicionar(Consulta consulta)
+        {
+            dataContext.Consultas.Add(consulta);
+            await dataContext.SaveChangesAsync();
+
+            return consulta;
+        }
+
+        public async Task<List<Consulta>> ObterTudo()
+        {
+            return await dataContext.Consultas.ToListAsync();
+        }
+    }
+}
