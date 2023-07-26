d07f6dc38284b6a9d971f8a40715e475ad6653c7
Paulo <pshenrique2015@gmail.com>
2023-07-26 14:16:40 -0300

    Implementando métodos da interface de Usuario repository.

--- PrevisaoDoTempoAPI/Repositories/UsuarioRepository.cs
+++ PrevisaoDoTempoAPI/Repositories/UsuarioRepository.cs
@@ -1,16 +1,48 @@
-﻿using PrevisaoDoTempoAPI.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using PrevisaoDoTempoAPI.Data;
 using PrevisaoDoTempoAPI.Interfaces;
 using PrevisaoDoTempoAPI.Models;
 
 namespace PrevisaoDoTempoAPI.Repositories
 {
-    public class UsuarioRepository
+    public class UsuarioRepository : IUsuarioRepository
     {
         private readonly DataContext dataContext;
 
         public UsuarioRepository(DataContext dataContext)
         {
             this.dataContext = dataContext;
+        }
+
+        public async Task<Usuario> Adicionar(Usuario usuario)
+        {
+            dataContext.Usuarios.Add(usuario);
+            await dataContext.SaveChangesAsync();
+
+            return usuario;
+        }
+
+        public async Task<bool> ExistePorLogin(string login)
+        {
+            return await dataContext.Usuarios.AnyAsync(u => u.Login.Equals(login));
+        }
+
+        /// <summary>
+        /// Retorna valor booleano para verificação da senha. 
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="senha"></param>
+        /// <returns>Retorna true se a senha estiver correta para aquele login, e false caso a senha esteja incorreta ou o login não exista.</returns>
+        public async Task<bool> SenhaCorretaPorLogin(string login, string senha)
+        {
+            Usuario? usuario = await dataContext.Usuarios.FindAsync(login);
+
+            if (usuario != null)
+            {
+               return usuario.Senha.Equals(senha);
+            }
+
+            return false;
         }
     }
 }
