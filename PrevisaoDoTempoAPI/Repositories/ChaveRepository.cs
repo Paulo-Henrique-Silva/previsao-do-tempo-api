c0c6c9703f0634f04ac79be1ffe532410bf2e661
Paulo <pshenrique2015@gmail.com>
2023-07-26 14:22:43 -0300

    Implementando métodos da classe chave repository.

---
+++ PrevisaoDoTempoAPI/Repositories/ChaveRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrevisaoDoTempoAPI.Data;
+using PrevisaoDoTempoAPI.Interfaces;
+using PrevisaoDoTempoAPI.Models;
+
+namespace PrevisaoDoTempoAPI.Repositories
+{
+    public class ChaveRepository : IChaveRepository
+    {
+        private readonly DataContext dataContext;
+
+        public ChaveRepository(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task<Chave> Adicionar(Chave chave)
+        {
+            dataContext.Chaves.Add(chave);
+            await dataContext.SaveChangesAsync();
+
+            return chave;
+        }
+
+        public async Task<List<Chave>> ObterChavesPorUsuarioId(uint usuarioId)
+        {
+            return await dataContext.Chaves.Where(c => c.UsuarioId == usuarioId).ToListAsync();
+        }
+
+        public async Task<List<Chave>> ObterTudo()
+        {
+            return await dataContext.Chaves.ToListAsync();
+        }
+    }
+}
