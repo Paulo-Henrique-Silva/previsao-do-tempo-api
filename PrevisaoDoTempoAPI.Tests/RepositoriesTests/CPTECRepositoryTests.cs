d0907f5528a0481ca6903939541fbf5c504cc6c0
Paulo <pshenrique2015@gmail.com>
2023-07-27 15:36:37 -0300

    Adicionando testes aos repositórios.

---
+++ PrevisaoDoTempoAPI.Tests/RepositoriesTests/CPTECRepositoryTests.cs
@@ -0,0 +1,40 @@
+﻿using PrevisaoDoTempoAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrevisaoDoTempoAPI.Tests.RepositoriesTests
+{
+    public class CPTECRepositoryTests
+    {
+        [Fact]
+        public void ObterCodigoCidadePorNomeEUF_Deve_Retornar_Codigo_Correto()
+        {
+            string nome = "sao paulo";
+            string uf = "SP";
+            uint codigoEsperado = 244;
+            CPTECRepository repository = new CPTECRepository();
+
+            var codigoObtido = repository.ObterCodigoCidadePorNomeEUF(nome, uf).Result;
+
+            Assert.Equal(codigoEsperado, codigoObtido);
+        }
+
+        [Fact]
+        public void ObterCodigoCidadePorNomeEUF_Deve_Retornar_0()
+        {
+            string nome = "São Paulo"; //query inválida por conter assentos no nome.
+            string uf = "SP";
+            uint codigoEsperado = 244;
+            CPTECRepository repository = new CPTECRepository();
+
+            var codigoObtido = repository.ObterCodigoCidadePorNomeEUF(nome, uf).Result;
+
+            Assert.NotEqual(codigoEsperado, codigoObtido);
+            Assert.Equal(0u, codigoObtido);
+        }
+    }
+}
--- PrevisaoDoTempoAPI.Tests/RepositoriesTests/ViaCEPRepositoryTests.cs
+++ PrevisaoDoTempoAPI.Tests/RepositoriesTests/ViaCEPRepositoryTests.cs
@@ -1,11 +1,39 @@
+using PrevisaoDoTempoAPI.Models;
+using PrevisaoDoTempoAPI.Repositories;
+
 namespace PrevisaoDoTempoAPI.Tests.RepositoriesTests
 {
     public class ViaCEPRepositoryTests
     {
         [Fact]
-        public void Test1()
+        public void ObterLocalizacaoPorCep_Deve_Retornar_Dados_Corretos()
         {
+            string cep = "01101-000"; //CEP da estação da Luz, em São Paulo.
+            ViaCEPRepository repository = new ViaCEPRepository();
+            Localizacao localizacao = new Localizacao()
+            {
+                Cep = cep,
+                Localidade = "São Paulo",
+                Uf = "SP"
+            };
 
+            var localizacaoResposta = repository.ObterLocalizacaoPorCep(cep).Result;
+
+            Assert.NotNull(localizacaoResposta);
+            Assert.Equal(localizacao.Cep, localizacaoResposta.Cep);
+            Assert.Equal(localizacao.Localidade, localizacaoResposta.Localidade);
+            Assert.Equal(localizacao.Uf, localizacaoResposta.Uf);
+        }
+
+        [Fact]
+        public void ObterLocalizacaoPorCep_Deve_Retornar_Nulo()
+        {
+            string cep = "01101-1345"; //CEP inválido.
+            ViaCEPRepository repository = new ViaCEPRepository();
+
+            var localizacaoResposta = repository.ObterLocalizacaoPorCep(cep).Result;
+
+            Assert.Null(localizacaoResposta);
         }
     }
 }
